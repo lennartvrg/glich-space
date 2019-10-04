686d8b826fa0f2020e98c05afc506847338d6fe5
Lennart Voorgang <lennart@voorgang.eu>
2019-10-04 15:37:41 +0200

    Lever animation

---
+++ Assets/Scripts/LeverBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverBehaviour : MonoBehaviour
+{
+    private Animator _animation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _animation.Play("Lever_PullDown");
+        }
+    }
+}
