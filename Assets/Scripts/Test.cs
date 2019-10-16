20aa75c68b011e9e2a8aa7a0c86175166b46ebc0
Luca Brües <luca@brues.de>
2019-10-16 18:12:46 +0200

    Implemented the VRTK Camera Rig and button, cube that are interactable

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        testLog();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void testLog()
+    {
+        Debug.Log("Its working!");
+    }
+}
