ff07550db8fb7ac1e5afff0208e26db430bf860b
Moritz Eich <moritz.eich@joblift.de>
2019-10-17 14:20:36 +0200

    first commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKey(KeyCode.E))
+        {
+            EditorApplication.isPlaying = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (Cursor.visible) {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }   
+}
