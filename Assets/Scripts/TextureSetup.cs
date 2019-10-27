b1543e0dc25f4e3b5e30571af3561527259468bd
Lennart Voorgang <lennart@voorgang.eu>
2019-10-28 00:53:37 +0100

    Getting oculus to run

--- Assets/Scripts/TextureSetup.cs
+++ Assets/Scripts/TextureSetup.cs
@@ -2,18 +2,21 @@
 
 public class TextureSetup : MonoBehaviour
 {
-
     public Camera secondCamera;
+    
     public Material secondCameraMaterial;
     
     // Start is called before the first frame update
     void Start()
     {
-        if (secondCamera.targetTexture != null)
-        {
-            secondCamera.targetTexture.Release();
-        }
-        secondCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB64);
+        secondCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
         secondCameraMaterial.mainTexture = secondCamera.targetTexture;
+    }
+
+    private void OnDestroy()
+    {
+        secondCamera.targetTexture.Release();
+        secondCamera.targetTexture = null;
+        secondCameraMaterial.mainTexture = null;
     }
 }
