9bbc19713387cbf3e2b0c129ac924726338e3081
Luca Brües <luca@brues.de>
2019-12-03 11:09:46 +0100

    Zombie disapperas

--- Assets/Scripts/LookAtObject.cs
+++ Assets/Scripts/LookAtObject.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class LookAtObject : MonoBehaviour
 { 
-    public float maxDistance = 100;
+    public float maxDistance = 1;
     private bool _isCounting = false;
     public float countdown = 3;
     public float time = 3;
     private RaycastHit _hit;
     private bool finished = false;
     public ElevatorBehaviour elevator;
+    public float zCountdown = 4;
+    private bool zCounting = false;
 
     public GameObject zombie;
     private Renderer zombieRend;
@@ -25,6 +27,7 @@ public class LookAtObject : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.forward, out _hit, maxDistance) && _hit.transform.CompareTag("Mirror"))
         {
             _isCounting = true;
+            Debug.Log("Counting");
         }
         else
         {
@@ -34,17 +37,30 @@ public class LookAtObject : MonoBehaviour
 
         if (countdown <= 0)
         {
+            Debug.Log("Läuft");
             //Zombie Behaviour
             zombieRend = zombie.GetComponent<Renderer>();
             zombieRend.enabled = true;
+            zCounting = true;
             
             //Elevator Behaviour
-            Debug.Log("BumBum");
             elevator.SetPower(true);
-            finished = true;
-
+            
         }
 
+        if (zCounting)
+        {
+            Debug.Log("Zombie Count");
+            countdown = 500;
+            zCountdown -= Time.deltaTime;
+            if (zCountdown <= 0)
+            {
+                Debug.Log("Zombie Done");
+                zombieRend.enabled = false;
+                finished = true;
+            }
+        }
+        
         if (_isCounting)
         {
             countdown -= Time.deltaTime;
